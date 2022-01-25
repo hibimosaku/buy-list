a842f663adbaf252cdefdf8406263c83abe22356
hibimosaku <n.kubomitsu@gmail.com>
2022-01-26 06:15:03 +0900

    #issu1 shims.vue-d.ts修正

--- types/shims-vue.d.ts
+++ types/shims-vue.d.ts
@@ -1,31 +1,30 @@
 /* eslint-disable */
-// import vue from "vue"
-import { Store } from "vuex";
-import { State } from "../src/store/store";
+// import { Store } from "vuex";
+// import { State } from "../src/store/store";
 
-declare module '@vue/runtime-core' {
+// declare module '@vue/runtime-core' {
   // declare your own store states
   // provide typings for `this.$store`
-  interface ComponentCustomProperties {
-    $store: Store<State>
-  }
-}
-
-
-// declare module "*.vue" {
-//   import type { DefineComponent } from "vue";
-//   const component: DefineComponent<{}, {}, any>;
-//   export default component;
-//   // interface Vue {
-//   //   $store: Vuex.store<any>, // Store が any になっているのでこの部分は妥協する
-//   //   $state: Store.State // State はこうすることによって型定義が守られる
-//   // }
-//   // interface ComponentCustomProperties {
-//   //   $store: Store<State>
-//   // }
+//   interface ComponentCustomProperties {
+//     $store: Store<State>
+//   }
 // }
-declare module 'vue/types/vue' {//かきたす
-  interface Vue {
-    $store: any
-  }
+
+
+declare module "*.vue" {
+  import type { DefineComponent } from "vue";
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
+  // interface Vue {
+  //   $store: Vuex.store<any>, // Store が any になっているのでこの部分は妥協する
+  //   $state: Store.State // State はこうすることによって型定義が守られる
+  // }
+  // interface ComponentCustomProperties {
+  //   $store: Store<State>
+  // }
 }
+// declare module 'vue/types/vue' {//かきたす
+//   interface Vue {
+//     $store: any
+//   }
+// }
